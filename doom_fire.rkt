44c706fc0ce2de89920422be0fe607064da592d1
Emmanuel Touzery <etouzery@gmail.com>
2019-04-07 20:45:23 +0200

    some work to allow the fire to shut down from the bottom. the animation at the beginning is no good though. the shutting down looks amazing!!

--- doom_fire.rkt
+++ doom_fire.rkt
@@ -9,95 +9,133 @@
 (define canvas-height 168)
 
 (define colors
-   (list 
-    "black"
-    (make-color  #x07 #x07 #x07)
-    (make-color  #x1F #x07 #x07)
-    (make-color  #x2F #x0F #x07)
-    (make-color  #x47 #x0F #x07)
-    (make-color  #x57 #x17 #x07)
-    (make-color  #x67 #x1F #x07)
-    (make-color  #x77 #x1F #x07)
-    (make-color  #x8F #x27 #x07)
-    (make-color  #x9F #x2F #x07)
-    (make-color  #xAF #x3F #x07)
-    (make-color  #xBF #x47 #x07)
-    (make-color  #xC7 #x47 #x07)
-    (make-color  #xDF #x4F #x07)
-    (make-color  #xDF #x57 #x07)
-    (make-color  #xDF #x57 #x07)
-    (make-color  #xD7 #x5F #x07)
-    (make-color  #xD7 #x5F #x07)
-    (make-color  #xD7 #x67 #x0F)
-    (make-color  #xCF #x6F #x0F)
-    (make-color  #xCF #x77 #x0F)
-    (make-color  #xCF #x7F #x0F)
-    (make-color  #xCF #x87 #x17)
-    (make-color  #xC7 #x87 #x17)
-    (make-color  #xC7 #x8F #x17)
-    (make-color  #xC7 #x97 #x1F)
-    (make-color  #xBF #x9F #x1F)
-    (make-color  #xBF #x9F #x1F)
-    (make-color  #xBF #xA7 #x27)
-    (make-color  #xBF #xA7 #x27)
-    (make-color  #xBF #xAF #x2F)
-    (make-color  #xB7 #xAF #x2F)
-    (make-color  #xB7 #xB7 #x2F)
-    (make-color  #xB7 #xB7 #x37)
-    (make-color  #xCF #xCF #x6F)
-    (make-color  #xDF #xDF #x9F)
-    (make-color  #xEF #xEF #xC7)
-    "white"))
+  (list 
+   "black"
+   (make-color  #x07 #x07 #x07)
+   (make-color  #x1F #x07 #x07)
+   (make-color  #x2F #x0F #x07)
+   (make-color  #x47 #x0F #x07)
+   (make-color  #x57 #x17 #x07)
+   (make-color  #x67 #x1F #x07)
+   (make-color  #x77 #x1F #x07)
+   (make-color  #x8F #x27 #x07)
+   (make-color  #x9F #x2F #x07)
+   (make-color  #xAF #x3F #x07)
+   (make-color  #xBF #x47 #x07)
+   (make-color  #xC7 #x47 #x07)
+   (make-color  #xDF #x4F #x07)
+   (make-color  #xDF #x57 #x07)
+   (make-color  #xDF #x57 #x07)
+   (make-color  #xD7 #x5F #x07)
+   (make-color  #xD7 #x5F #x07)
+   (make-color  #xD7 #x67 #x0F)
+   (make-color  #xCF #x6F #x0F)
+   (make-color  #xCF #x77 #x0F)
+   (make-color  #xCF #x7F #x0F)
+   (make-color  #xCF #x87 #x17)
+   (make-color  #xC7 #x87 #x17)
+   (make-color  #xC7 #x8F #x17)
+   (make-color  #xC7 #x97 #x1F)
+   (make-color  #xBF #x9F #x1F)
+   (make-color  #xBF #x9F #x1F)
+   (make-color  #xBF #xA7 #x27)
+   (make-color  #xBF #xA7 #x27)
+   (make-color  #xBF #xAF #x2F)
+   (make-color  #xB7 #xAF #x2F)
+   (make-color  #xB7 #xB7 #x2F)
+   (make-color  #xB7 #xB7 #x37)
+   (make-color  #xCF #xCF #x6F)
+   (make-color  #xDF #xDF #x9F)
+   (make-color  #xEF #xEF #xC7)
+   "white"))
 
 (define color-count (length colors))
 
-(define (compute-line depth previous-line)
+(define (compute-line previous-line orig-line)
   ;; mapping on multiple lists == zipping in other languages
   ;; zipping with the list moved to the left & right to get
   ;; the previous & next items
-  (map (λ (previous-before previous previous-after)
-         (define x (case (random 3)
-                     [(0) previous-before]
-                     [(2) previous-after]
-                     [else previous]))
-         (if (eq? (random 2) 1)
-             (max 0 (if (< (sub1 x) (- color-count depth 1)) 0 (sub1 x)))
-             x))
-       (append (list (first previous-line)) (drop-right previous-line 1)) ;; drop-right & append not ideal for perf
-       previous-line
-       (append (drop previous-line 1) (list (first previous-line))))) ;; append not ideal for perf
+  (map
+   (λ (previous-before previous previous-after orig-color)
+     (define x0
+       (case (random 3)
+         [(0) previous-before]
+         [(2) previous-after]
+         [else previous]))
+     (define x1
+       (if (eq? (random 2) 1)
+           (max 0 (sub1 x0))
+           x0))
+     ;; special case if we compute 0, don't necessarily stay black,
+     ;; maybe the flame can keep running if it was running in
+     ;; the previous frame. allows the frame to have vertical "holes"
+     ;; and for a VERY NICE effect when shutting down the fire from
+     ;; the bottom.
+     (max 0 (if (eq? x1 0)
+         (case (random 2)
+           [(0) (quotient orig-color 2)]
+           [(1) (sub1 orig-color)])
+         x1)))
+   (append
+    (list (first previous-line))
+    (drop-right previous-line 1)) ;; drop-right & append not ideal for perf
+   previous-line
+   (append
+    (drop previous-line 1)
+    (list (first previous-line)))
+   orig-line)) ;; append not ideal for perf
 
-(define (compute-frame depth)
-  (reverse (for/fold ([frame (list (start-line))])
-            ([i (in-range 0 (sub1 canvas-height))])
-    #:break (andmap zero? (first frame))
-    (cons (compute-line depth (first frame)) frame))))
+(define (compute-next-frame orig-frame)
+  (define new-start
+    (for/fold
+     ([frame (list (first orig-frame))])
+     ([orig-line orig-frame])
+      (cons (compute-line (first frame) orig-line) frame)))
+  (reverse
+   (if (>= (length new-start) (length orig-frame))
+       new-start
+       (append
+        (reverse (drop orig-frame (length new-start)))
+        new-start))))
 
-(define (paint-line line y start-canvas)
-  (for/fold ([canvas start-canvas]) ([pixel line] [x (in-naturals)])
+(define (paint-line line start-canvas)
+  (for/fold
+   ([canvas start-canvas])
+   ([pixel line] [x (in-naturals)])
     (cons (list-ref colors pixel) canvas)))
 
-(define (draw-flames depth)
-  (define frame (compute-frame depth))
+(define (draw-flames frame)
   (define start-canvas (list))
-  (define flames (~>
-                  (for/fold ([canvas start-canvas])
-                            ([i (in-naturals)]
-                             [line frame])
-                    (paint-line line (- canvas-height (- depth i)) canvas))
-                  (color-list->bitmap _ canvas-width (length frame))))
+  (define flames
+    (~>
+     (for/fold
+      ([canvas start-canvas])
+      ([i (in-naturals)]
+       [line frame])
+       (paint-line line canvas))
+     (color-list->bitmap _ canvas-width (length frame))))
   (define full-canvas
     (rectangle canvas-width canvas-height "solid" "black"))
-  (place-image/align flames 0 (- canvas-height (length frame)) "left" "top" full-canvas))
+  (place-image/align
+   flames 0
+   (- canvas-height (length frame))
+   "left" "top" full-canvas))
 
 ;; our state will be the rows to be displayed,
 ;; containing not colors but indexes of colors
 ;; at the beginning I start with a single line of
 ;; white
-(define (start-line)
-  (build-list canvas-width (const (sub1 color-count))))
+(define (line-col col)
+  (build-list canvas-width (const col)))
 
-(big-bang 0
-          (on-tick add1 .1) ;; .1 => 10fps (otherwise it's too fast)
+(define (start-line)
+  (line-col (sub1 color-count)))
+
+(big-bang (list (start-line))
+          (on-key (λ (st key)
+                    (cons
+                     (line-col 0)
+                     (drop st 1))))
+          (on-tick compute-next-frame 0.1)
           (on-draw draw-flames))
 
